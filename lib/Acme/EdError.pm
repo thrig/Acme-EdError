a458387e2d5df25dda8175f6ff6e1cdf86855e36
Jeremy Mates <jmates@sial.org>
2009-04-30 19:13:55 +0000

    Initial work.

---
+++ lib/Acme/EdError.pm
@@ -0,0 +1,56 @@
+# Implements ed(1) error handling. Run perldoc(1) on this module for
+# additional documentation.
+#
+# Copyright 2009 by Jeremy Mates.
+#
+# This module is free software; you can redistribute it and/or modify it
+# under the Artistic license.
+
+package Acme::EdError;
+
+$VERSION = '0.01';
+
+$SIG{__DIE__}  = sub { print STDERR "?\n"; exit 255 };
+$SIG{__WARN__} = sub { print STDERR "?\n"; };
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::EdError - implements ed(1) error handling
+
+=head1 SYNOPSIS
+
+  use Acme::EdError;
+
+  warn "uh oh";
+  die  "oh well";
+
+=head1 DESCRIPTION
+
+This module implements ed(1) error handling in perl, eliminating
+needless verbosity from error messages. To quote the ed(1) manual:
+
+  "When an error occurs, ed prints a `?' and either returns to command
+  mode or exits if its input is from a script. An explanation of the
+  last error can be printed with the `h' (help) command."
+
+Help support has not yet been implemented in this module.
+
+=head1 SEE ALSO
+
+ed(1)
+
+=head1 AUTHOR
+
+Jeremy Mates, E<lt>jmates@sial.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2009 by Jeremy Mates.
+
+This module is free software; you can redistribute it and/or modify it
+under the Artistic license.
+
+=cut
